986babd18ff0d5973bbd2206b15fdac5bcdb9b93
Andres Daza <cdazaparra@gmail.com>
2023-01-23 15:56:15 -0500

    pull

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Contador from "./components/Contador";
+import ContadorMejorado from "./components/ContadorMejorado";
+import ShoppingCard from "./components/ShoppingCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Contador />
+      <hr></hr>
+      <ContadorMejorado></ContadorMejorado>
+      <hr></hr>
+      <ShoppingCard></ShoppingCard>
+    </>
   );
 }
 
